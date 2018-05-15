4e5ad267cee436f1c4a7bce9b12f459b19bae198
AriHarry <34508094+AriHarry@users.noreply.github.com>
2018-05-15 11:52:40 +0530

    Add files via upload

---
+++ ClsProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginInMVC4WithEF.Models
+{
+    public class ClsProduct
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+         
+        public string Name { get; set; }
+        
+        public string ModelPlace { get; set; }
+         
+        public string Rate { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Department> lstProduct = new List<Department>();
+    }
+    public class Department
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+    }
+}
